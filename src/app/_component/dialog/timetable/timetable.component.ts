824e403b87e8a4f5687f7a63b8c4ecbfb0b091e6
Tomasz Zarówny <t.zarowny@gmail.com>
2020-12-01 20:44:30 +0100

    timetab cd

--- src/app/_component/dialog/timetable/timetable.component.ts
+++ src/app/_component/dialog/timetable/timetable.component.ts
@@ -1,10 +1,13 @@
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpEvent } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { GroupInstrument } from './../../../_models/group';
 import { GroupService } from './../../../_services/group.service';
 import { Inspection } from './../../../_models/inspection';
 import { Instrument } from './../../../_models/instrument';
 import { InstrumentService } from 'src/app/_services';
 import { CertificateService } from './../../../_services/certificate.service';
-import { Component, Inject, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild, AfterViewInit, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { add, subtract } from 'add-subtract-date';
 import { DatePipe } from '@angular/common';
@@ -12,15 +15,17 @@ import { DateAssistant } from 'src/app/_helpers';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { AfterViewChecked } from '@angular/core';
 
 @Component({
   templateUrl: './timetable.component.html',
   styleUrls: ['./timetable.component.css']
 })
-export class TimetableComponent implements OnInit, AfterViewInit {
+export class TimetableComponent implements OnInit, AfterViewInit, AfterViewChecked, DoCheck {
 
   startDate: Date;
   endDate: Date;
+  yearForm: FormGroup;
   selectedValue: Date;
   yearsList: Array<Date> = [];
   instrumentList: Instrument[];
@@ -46,17 +51,30 @@ export class TimetableComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     // this.paginator._intl.itemsPerPageLabel = 'Wyników na stronie:';
     this.dataSource.paginator = this.paginator;
-
+    console.log('AfterViewInit');
   }
+  ngAfterViewChecked() {
+    // console.log('ngAfterViewChecked');
+  }
+
+  ngDoCheck(): void{
+    console.log('ngDoCheck');
+  }
+
+  get year() { return this.yearForm.get('year'); }
 
   ngOnInit(): void {
     this.dialogRef.updateSize('1000px', '800px');
     this.dialogRef.updatePosition({top: '30px'});
+    this.yearForm = new FormGroup({
+      year: new FormControl(new Date())
+    });
     const dateStart = new Date('2020-01-01');
     const dateCurr = new Date();
     const years = Math.floor((dateCurr.getTime() - dateStart.getTime()) / 1000 / 60 / 60 / 24 / 365);
     let d = new Date(dateStart.getTime());
     this.selectedValue = d;
+    this.startDate = this.selectedValue;
     for (let i = 0; i < 11 + years; i++){
       this.yearsList.push(d);
       d = new Date(d.getTime());
@@ -65,13 +83,31 @@ export class TimetableComponent implements OnInit, AfterViewInit {
     // console.log(this.yearsList);
     this.groupService.getList().subscribe( group => {
       this.groupList = group;
-      this.refresh();
+      console.log('załadowałem grupy');
+      this.yearForm.controls.year.valueChanges.subscribe( val => {
+        this.readyToView = false;
+        console.log('była zmiana1');
+        console.log(this.selectedValue);
+        setTimeout(() => {
+          this.refresh();
+        }, 1000);
+        // this.refresh();
+      });
+      this.year.setValue(this.selectedValue);
+      console.log(this.selectedValue);
     });
+
   }
-  refresh(){
-    // console.log('refresh:');
-    // console.log(this.selectedValue);
+  buttClick(){
+    // this.year.setValue(new Date('2021-01-01'));
+    // this.readyToView = !this.readyToView;
     this.readyToView = false;
+    this.refresh();
+  }
+
+  public refresh(){
+    // console.log('refresh:');
+    console.log(this.selectedValue);
     this.startDate = this.selectedValue;
     this.endDate = new Date(this.selectedValue.getFullYear() + '-12-31');
     const param = '?max_st=3';
@@ -86,8 +122,61 @@ export class TimetableComponent implements OnInit, AfterViewInit {
           const certFilter = cert.filter(x => x.przedmiotId.id === instrElement.id);
 
           certFilter.forEach( certElement => {
+            let insp = new Inspection();
+            let d = new Date(certElement.dataSprawdzenia);
+            switch (d.getMonth()){
+              case 0: {
+                        insp.dataPlanowa0 = d;
+                        break;
+                      }
+              case 1: {
+                        insp.dataPlanowa1 = d;
+                        break;
+                      }
+              case 2: {
+                        insp.dataPlanowa2 = d;
+                        break;
+                      }
+              case 3: {
+                        insp.dataPlanowa3 = d;
+                        break;
+                      }
+              case 4: {
+                        insp.dataPlanowa4 = d;
+                        break;
+                      }
+              case 5: {
+                        insp.dataPlanowa5 = d;
+                        break;
+                      }
+              case 6: {
+                        insp.dataPlanowa6 = d;
+                        break;
+                      }
+              case 7: {
+                        insp.dataPlanowa7 = d;
+                        break;
+                      }
+              case 8: {
+                        insp.dataPlanowa8 = d;
+                        break;
+                      }
+              case 9: {
+                        insp.dataPlanowa9 = d;
+                        break;
+                      }
+              case 10: {
+                        insp.dataPlanowa10 = d;
+                        break;
+                      }
+              case 11: {
+                        insp.dataPlanowa11 = d;
+                        break;
+                      }
+            }
+            insp.nrSwiadectwa = certElement.nrSwiadectwa;
             instrElement.sprawdzeniaPlanowe
-            .push({dataPlanowa: new Date(certElement.dataSprawdzenia), nrSwiadectwa: certElement.nrSwiadectwa});
+            .push(insp);
           });
 
           if (instrElement.dataNastepnejKontroli){
@@ -102,7 +191,7 @@ export class TimetableComponent implements OnInit, AfterViewInit {
         console.log(this.instrumentList);
         // console.log(cert);
         this.dataSource.data = this.instrumentList;
-
+        this.readyToView = true;
 
       });
     });
@@ -119,9 +208,7 @@ export class TimetableComponent implements OnInit, AfterViewInit {
 
     //   return `${startIndex + 1} – ${endIndex} z ${length}`;
     // };
-    this.dataSource.paginator = this.paginator;
-    console.log(this.paginator);
-    this.readyToView = true;
+
   }
 
   propDate(grupa: GroupInstrument, firstDate: Date, dataStart: Date, dataEnd: Date): Inspection[]{
--- src/app/_models/inspection.ts
+++ src/app/_models/inspection.ts
@@ -2,6 +2,18 @@ export class Inspection {
   id?: number | null;
   idPrzyrzad?: number | null;
   dataPlanowa?: Date | null;
+  dataPlanowa0?: Date | null;
+  dataPlanowa1?: Date | null;
+  dataPlanowa2?: Date | null;
+  dataPlanowa3?: Date | null;
+  dataPlanowa4?: Date | null;
+  dataPlanowa5?: Date | null;
+  dataPlanowa6?: Date | null;
+  dataPlanowa7?: Date | null;
+  dataPlanowa8?: Date | null;
+  dataPlanowa9?: Date | null;
+  dataPlanowa10?: Date | null;
+  dataPlanowa11?: Date | null;
   nrSwiadectwa?: string | null;
 }
 
